80b9a4524c6fe9c5c6f0c608a00f3d973347ceed
Maria Situmbeko <mariasitumbeko@gmail.com>
2019-06-07 16:43:07 +0200

    First NativeScript App commit

---
+++ src/app/todo/todo.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector: "Todo",
+    moduleId: module.id,
+    templateUrl: "./todo.component.html",
+    styleUrls: ["./todo.component.css"]
+})
+export class TodoComponent implements OnInit {
+
+    constructor() {
+    }
+
+    ngOnInit(): void {
+    }
+}
