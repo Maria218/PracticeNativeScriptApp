e0f8850ea79c6b33872f38ee148a0e43acad545a
Maria Situmbeko <mariasitumbeko@gmail.com>
2019-06-09 09:34:24 +0200

    Added a login page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from "@angular/core";
 
 @Component({
-    selector: "ns-app",
-    templateUrl: "app.component.html"
+  selector: "gr-login",
+  templateUrl: "login/login.component.html"
 })
-export class AppComponent { }
+export class AppComponent {}
