7513a18d8d23ad66d3d3e3518d47443357f620b2
Maria Situmbeko <mariasitumbeko@gmail.com>
2019-06-09 18:04:32 +0200

    Added the shared folder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,21 @@
 import { Component } from "@angular/core";
+import { User } from "./shared/user/user.model";
 
 @Component({
   selector: "gr-login",
   templateUrl: "login/login.component.html",
   styleUrls: ["login/login.component.css"]
 })
-export class AppComponent {}
+
+export class AppComponent {
+  email = "mariasitumbeko@gmail.com";
+  isLoggingIn = true;
+
+  submit() {
+    alert("You're using: " + this.email);
+  }
+
+  toggleDisplay() {
+    this.isLoggingIn = !this.isLoggingIn;
+  }
+}
